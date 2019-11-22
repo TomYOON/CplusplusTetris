d29c7f70fe6aacaeb8328c682c0950e072af131b
kdje0506 <48521269+kdje0506@users.noreply.github.com>
2019-11-22 10:14:44 +0900

    add Container class

---
+++ c++Tetis/Container.cpp
@@ -0,0 +1,29 @@
+#include "Container.h"
+#include <Windows.h>
+
+Container::Container()
+	:Container(0,0)
+{
+
+}
+
+Container::Container(const int& ab_x, const int& ab_y)
+:m_ab_x(ab_x),m_ab_y(ab_y)
+{
+}
+
+void Container::setColor(const int& color)
+{
+	static HANDLE std_output_handle;
+	std_output_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(std_output_handle, color);
+}
+
+void Container::gotoxy(const int& x, const int& y)
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD pos;
+	pos.Y = y;
+	pos.X = x;
+	SetConsoleCursorPosition(hConsole, pos);
+}
