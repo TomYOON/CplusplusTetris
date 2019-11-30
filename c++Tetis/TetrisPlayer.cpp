0094d2a1fd9cf3eba8c4237cd723f3a10423359b
kdje0506 <48521269+kdje0506@users.noreply.github.com>
2019-11-30 12:09:50 +0900

    tp 원형

--- c++Tetis/TetrisPlayer.cpp
+++ c++Tetis/TetrisPlayer.cpp
@@ -1,1 +1,61 @@
 #include "TetrisPlayer.h"
+
+void TetrisPlayer::init()
+{
+}
+
+int TetrisPlayer::getKeyUp()
+{
+	return 0;
+}
+
+int TetrisPlayer::getKeyDown()
+{
+	return 0;
+}
+
+int TetrisPlayer::getKeyLeft()
+{
+	return 0;
+}
+
+int TetrisPlayer::getKeyRight()
+{
+	return 0;
+}
+
+int TetrisPlayer::getKeySpace()
+{
+	return 0;
+}
+
+int TetrisPlayer::showScreen(int speed)
+{
+	return 0;
+}
+
+void TetrisPlayer::setSpeed(int s)
+{
+}
+
+bool TetrisPlayer::isClear(int l)
+{
+	return false;
+}
+
+bool TetrisPlayer::isGameOver(int igo)
+{
+	return false;
+}
+
+TetrisPlayer::TetrisPlayer()
+{
+}
+
+TetrisPlayer::~TetrisPlayer()
+{
+}
+
+void TetrisPlayer::run()
+{
+}
--- c++Tetis/TetrisPlayer.h
+++ c++Tetis/TetrisPlayer.h
@@ -1,6 +1,35 @@
 #pragma once
+#include "Stage.h"
+
 class TetrisPlayer
 {
-	
+private:
+	const unsigned int m_EXT_KEY = 0xffffffe0;
+	const int m_KEY_LEFT = 0x4b;
+	const int  m_KEY_RIGHT = 0x4d;
+	const int  m_KEY_UP = 0x48;
+	const int  m_KEY_DOWN = 0x50;
+	Stage m_stage;
+	GameContainer* [] m_gcArray;
+	int m_frame;
+	char m_keytemp;
+	int m_is_gameOver;
+	int m_speed;
+	void init();
+	int getKeyUp();
+	int getKeyDown();
+	int getKeyLeft();
+	int getKeyRight();
+	int getKeySpace();
+	int showScreen(int speed);
+	void setSpeed(int s);
+	bool isClear(int l);
+	bool isGameOver(int igo);
+
+public:
+	TetrisPlayer();
+	~TetrisPlayer();
+	void run();
+
 };
 
