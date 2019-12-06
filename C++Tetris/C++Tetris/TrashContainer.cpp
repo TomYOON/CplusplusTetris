1c49dd7cb11421293962514c0870ef4634ea6220
asia0413 <57991519+asia0413@users.noreply.github.com>
2019-12-07 02:30:27 +0900

    TrashContiainer

--- C++Tetris/C++Tetris/TrashContainer.cpp
+++ C++Tetris/C++Tetris/TrashContainer.cpp
@@ -200,14 +200,14 @@ void TrashContainer::show_gamestat()
 		//cout << "SCORE";
 
 		gotoxy(m_ab_x + 30, m_ab_y + 15);  // 35 12
-		cout << "남은 쓰레기 양";
+		cout << "남은 수거 횟수";
 
 
 	}
 	gotoxy(m_ab_x + 30, m_ab_y + 10); // 41 7
-	cout << "trashContainer.";
+	cout << "우주쓰레기 파편들을 차곡차곡 정리하자!";
 	//gotoxy(35, 10);
 	//cout << m_score;
 	gotoxy(m_ab_x + 30, m_ab_y + 17); // 35 13
-	cout << stage_data[m_level].get_clear_line() - m_lines << " KG";
+	cout << stage_data[m_level].get_clear_line() - m_lines << " 회";
 }
